5a5551d1b508128497bdc41969b97d9d216c71f9
Nadir Kalajdzic <nkalajdzic1@etf.unsa.ba>
2021-06-24 16:38:24 +0200

    modified run start script

--- server/server.js
+++ server/server.js
@@ -1,22 +1,20 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+const db = require("./database.js");
+const path = require("path");
+const fs = require("fs");
 const app = express();
-const db = require('./database.js');
-const path = require('path');
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '..', 'client', 'src')));
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'client', 'src', 'index.tsx'));
-});
 
 //routes
-app.use('/auction', require('./routes/auction.js'));
+app.use("/auction", require("./routes/auction.js"));
 
 //check connection to database
-db.authenticate().then(() => console.log('Connected to auctionDB database.'))
-    .catch(err => console.log(err));
+db.authenticate()
+  .then(() => console.log("Connected to auctionDB database."))
+  .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server listening on ${PORT}`));
+app.listen(PORT, async () => console.log(`Server listening on ${PORT}`));
