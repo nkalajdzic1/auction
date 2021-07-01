daebb3b15ab3575c2038a6dd26390a3b0fa1830e
Nadir Kalajdzic <nkalajdzic1@etf.unsa.ba>
2021-07-01 13:24:51 +0200

    ini

--- server/database.js
+++ server/database.js
@@ -8,6 +8,3 @@ module.exports = new Sequelize(process.env.CLEAR_DATABASE_URL, {
         idle: 10000
     },
 });
-//mysql://bf9029dad2acca:82022ccf@eu-cdbr-west-01.cleardb.com/heroku_1532f95a856551d?reconnect=true
-
-enviromental
